f84b857a1b92d4d7de2068113145e8aa714a2551
Poweramo <assirouomar@gmail.com>
2025-01-13 08:43:20 +0100

    feat: implement fibonacci using recursive

--- method1.js
+++ method1.js
@@ -1,3 +1,9 @@
+function fibonacci(n) {
+	if (n === 0 || n === 1) return 1;
+
+	return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
 const tests = [];
 for (let i = 0; i <= 100; i++) tests.push(i);
 const results = [];
